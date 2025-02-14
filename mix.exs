242b4d63db2713733ddd6482e3697a5f9c8b3d3a
Brian Underwood <public@brian-underwood.codes>
2025-02-14 21:46:48 +0100

    chore: Release fix to extra insert broadcast

--- mix.exs
+++ mix.exs
@@ -6,7 +6,7 @@ defmodule EctoWatch.MixProject do
   def project do
     [
       app: :ecto_watch,
-      version: "0.12.4",
+      version: "0.12.5",
       elixir: "~> 1.10",
       description:
         "EctoWatch allows you to easily get Phoenix.PubSub notifications directly from postgresql.",
